bd8fdadfd75d4e6ba0dd5254f6b7143cb81020ed
Ioannis Papikas <papikas.ioan@gmail.com>
2017-10-02 15:39:19 +0300

    Add HtmlTranslationParser

---
+++ src/parsers/html-translation-parser.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const fs = require('fs');
+const jsdom = require('jsdom');
+const PoObject = require('../po/po-object');
+const PoCollection = require('../po/po-collection');
+
+class HtmlTranslationParser {
+    constructor(options) {
+        this.options = options;
+        this.collection = new PoCollection();
+    }
+
+    /**
+     * Get all translation objects.
+     *
+     * @returns {PoCollection}
+     */
+    getCollection() {
+        return this.collection;
+    }
+
+    /**
+     * Parse Html file
+     *
+     * @return {Array}
+     * @param file
+     */
+    parseFile(file) {
+        // Get file contents
+        const fileContents = fs.readFileSync(file).toString();
+
+        // Parse html fragment using jsdom
+        const fragment = jsdom.JSDOM.fragment(fileContents);
+
+        // Get all tags with a data-translate attribute
+        let dataTranslateElements = fragment.querySelectorAll('[data-translate]');
+
+        for (const element of dataTranslateElements) {
+            // Get the translation key
+            let translationKey = element.getAttribute('data-translate');
+
+            // Get the translation comment
+            let translationComment = element.getAttribute('data-translate-comment');
+
+            // Create translation object
+            let translationObject = new PoObject({
+                filename: file,
+                comment: translationComment,
+                msgid: translationKey
+            });
+
+            // Append translation object
+            this.collection.addTranslation(translationObject);
+        }
+    }
+}
+
+module.exports = HtmlTranslationParser;
